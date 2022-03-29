83b7013dad81fe643cc7569d7e937023bdd2f647
taaaaa111 <taos.tou@gmail.com>
2022-03-29 20:39:28 +0200

    model added to mailer client and mailer doctor

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,2 +1,9 @@
 class ClientMailer < ApplicationMailer
-end
+    default from: 'no-reply@monsite.fr'
+
+    def welcome_email
+      @client = params[:client]
+      @url  = 'http://example.com/login'
+      mail(to: @client.email, subject: 'Bienvenue sur Animalib')
+    end
+  end
--- app/mailers/doctor_mailer.rb
+++ app/mailers/doctor_mailer.rb
@@ -1,2 +1,10 @@
 class DoctorMailer < ApplicationMailer
-end
+    default from: 'no-reply@monsite.fr'
+
+    def welcome_email
+      @doctor = params[:doctor]
+      @url  = 'http://example.com/login'
+      mail(to: @doctor.email, subject: 'Bienvenue sur Animalib')
+    end
+
+  end
