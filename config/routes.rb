88e1f7b9e9d3e7374793250ea19e2725c9b6b408
Camille-BAE <Camille.jbae@gmail.com>
2022-03-30 21:52:08 +0200

    update routes with the REST method

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :doctors
+  devise_for :clients
+  
+  resources :doctors
+
+  resources :clients do
+    resources :animals
+  end
+
+  resources :cities
+  resources :appointments
+
   get 'static_page/team'
   get 'static_page/contact'
   get 'static_page/cgu'
-  # devise_for :users
-  devise_for :doctors
-  devise_for :clients
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  
   root to: "welcome#index"
 end
