77c65f7098a901d3258684afad639261b41b333f
Camille-BAE <Camille.jbae@gmail.com>
2022-03-30 12:44:57 +0200

    test faker client and doctor

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+# Fake Doctor
+Doctor.destroy_all
+
+15.times do
+  Doctor.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6)
+  )
+end
+
+
+# Fake Client
+Client.destroy_all
+
+15.times do
+  Client.create(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6)
+  )
+end
