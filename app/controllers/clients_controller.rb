fb8c4d0304289799dc0fbc69a1048830bca40731
ChristelCOVI <christel.covi@gmail.com>
2022-03-31 00:50:12 +0200

    re update controller clients welcome doctors

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -7,21 +7,21 @@ class ClientsController < ApplicationController
     @profil_client = Profil_client.find(params[:id])
     @appointments = @profil_client.client.appointments
 =======
-    #@profil_client = Profil_client.find(params[:id])
-    #@appointmentss = @profil_client.client.appointments
+    @profil_client = Profil_client.find(params[:id])
+    @appointments = @profil_client.client.appointments
 >>>>>>> development
   end
 
   def edit
-    #@profil_client  = Profil_client.find(params[:id]) 
+    @profil_client  = Profil_client.find(params[:id]) 
   end
 
   def update
-    #@profil_client  = Profil_client.find(params[:id])  
-    #if @profil_client.update(profil_client_params)
-    #  redirect_to profil_client_path(@profil_client), notice: "Votre profil a bien été enregistré."
-    #else
-    #  render :edit, alert: "Veuillez rentrer des champs valides."
+    @profil_client  = Profil_client.find(params[:id])  
+    if @profil_client.update(profil_client_params)
+      redirect_to profil_client_path(@profil_client), notice: "Votre profil a bien été enregistré."
+    else
+      render :edit, alert: "Veuillez rentrer des champs valides."
   end
 
 <<<<<<< HEAD
@@ -31,11 +31,11 @@ class ClientsController < ApplicationController
     params.require(:profil_client).permit(:name, :city, :phone_number, :business_line)
   end
 =======
-  #private 
+  private 
   
-  #def profil_client_params
-    #params.require(:profil_client).permit(:name, :city, :phone_number, :business_line)
-  #end
+  def profil_client_params
+    params.require(:profil_client).permit(:name, :city, :phone_number, :business_line)
+  end
 >>>>>>> development
   
 end
