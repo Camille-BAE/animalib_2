6d1ccf66049868e90cf7754faf55489613c9ec4b
Camille-BAE <Camille.jbae@gmail.com>
2022-03-30 11:47:01 +0200

    updated table doctor and client. And add self.abstract_class for the inheritence STI

--- db/migrate/20220328105530_create_doctors.rb
+++ db/migrate/20220328105530_create_doctors.rb
@@ -1,6 +1,12 @@
 class CreateDoctors < ActiveRecord::Migration[5.2]
   def change
     create_table :doctors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :name_office
+      t.string :zip_code
+      t.integer :phone
+      t.text :description
 
       t.timestamps
     end
