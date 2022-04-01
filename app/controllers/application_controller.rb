b1966b13a0a8b729dd4f51d6b2ddf1fd0acde373
Camille-BAE <Camille.jbae@gmail.com>
2022-04-01 11:45:23 +0200

    put the method of application, clients, doctors controllers in-commentary and return in the last version of the app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
 
-  def after_sign_in_path_for(resource)
-    if @client
-      clients_path(@client.profil_client)
-    else @doctor
-      doctors_path(@doctor.profil_doctor)
-    end
-  end
+  #def after_sign_in_path_for(resource)
+    #if @client
+      #clients_path(@client.profil_client)
+    #else @doctor
+      #doctors_path(@doctor.profil_doctor)
+    #end
+  #end
 
 end
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -3,26 +3,26 @@ class ClientsController < ApplicationController
   #before_action :authenticate_client!
 
   def show
-    @profil_client = Profil_client.find(params[:id])
-    @appointments = @profil_client.client.appointments
+    #@profil_client = Profil_client.find(params[:id])
+    #@appointments = @profil_client.client.appointments
   end
 
   def edit
-    @profil_client  = Profil_client.find(params[:id]) 
+    #@profil_client  = Profil_client.find(params[:id]) 
   end
 
   def update
-    @profil_client  = Profil_client.find(params[:id])  
-    if @profil_client.update(profil_client_params)
-      redirect_to clients_path(@profil_client), notice: "Votre profil a bien été enregistré."
-    else
-      render :edit, alert: "Veuillez rentrer des champs valides."
+    #@profil_client  = Profil_client.find(params[:id])  
+    #if @profil_client.update(profil_client_params)
+    #  redirect_to clients_path(@profil_client), notice: "Votre profil a bien été enregistré."
+    #else
+    #  render :edit, alert: "Veuillez rentrer des champs valides."
   end
 
   private 
 
-  def profil_client_params
-    params.require(:profil_client).permit(:name, :city, :phone_number, :business_line)
-  end
+  #def profil_client_params
+    #params.require(:profil_client).permit(:name, :city, :phone_number, :business_line)
+  #end
   
 end
